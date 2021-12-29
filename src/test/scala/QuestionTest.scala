01843d32d1793a8e9ce3b5af1fe577a2127c7749
vardhan <vardhan5a7@gmail.com>
2021-12-29 21:11:31 +0530

    Initial commit

---
+++ src/test/scala/QuestionTest.scala
@@ -0,0 +1,12 @@
+import org.scalatest.funsuite.AnyFunSuite
+
+
+
+class QuestionTest extends AnyFunSuite {
+
+
+  println(Question1.countUniqueLocation)
+  println("fffsfsdf")
+
+  assert(Question1.countUniqueLocation() == 8)
+}
